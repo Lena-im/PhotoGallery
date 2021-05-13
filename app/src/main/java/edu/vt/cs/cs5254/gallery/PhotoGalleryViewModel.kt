1915d06d16d2a657df1244ddd74e8b26af8a850b
NAN LI <nanl@NANs-MacBook-Pro-2.local>
2021-05-13 11:52:37 -0400

    first commit

---
+++ app/src/main/java/edu/vt/cs/cs5254/gallery/PhotoGalleryViewModel.kt
@@ -0,0 +1,21 @@
+package edu.vt.cs.cs5254.gallery
+
+import android.graphics.drawable.Drawable
+import androidx.lifecycle.ViewModel
+
+class PhotoGalleryViewModel : ViewModel() {
+
+    val galleryItemsLiveData = FlickrFetchr.galleryItemsLiveData
+
+    fun loadPhotos() {
+        FlickrFetchr.fetchPhotos()
+    }
+
+    fun hardReloadPhotos(){
+        FlickrFetchr.loadPhotos()
+    }
+
+    fun storeThumbnail(id: String, drawable: Drawable) {
+        FlickrFetchr.storeThumbnail(id, drawable)
+    }
+}
